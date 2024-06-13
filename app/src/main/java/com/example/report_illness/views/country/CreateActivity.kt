8bed683f2c724e460d96690988bed1bc12a18321
Humberto Agudelo <humberto228xd@gmail.com>
2024-06-13 01:47:10 -0500

    Refactorización de Create Activity en Pais

--- app/src/main/java/com/example/report_illness/views/country/CreateActivity.kt
+++ app/src/main/java/com/example/report_illness/views/country/CreateActivity.kt
@@ -1,46 +1,57 @@
 package com.example.report_illness.views.country
 
-import android.annotation.SuppressLint
+
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Spinner
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.report_illness.R
 import com.example.report_illness.database.ConnectionDB
 import com.example.report_illness.helpers.CountryHelper
 import com.example.report_illness.models.Country
-import java.util.Calendar
+
 
 class CreateActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_country_create)
 
+
         ConnectionDB.dbConn()
-
         setupSubmitButton()
+        setupCancelButton()
+        populateFieldsIfEditing()
+    }
 
+
+    private fun setupCancelButton() {
         val buttonCancel = findViewById<Button>(R.id.buttonCancel)
         buttonCancel.setOnClickListener {
             finish()
         }
+    }
 
+
+    private fun populateFieldsIfEditing() {
         val isNewCountry = intent.getBooleanExtra("isNewCountry", true)
+
 
         if (!isNewCountry) {
             val editTextId = findViewById<EditText>(R.id.editTextId)
             editTextId.isEnabled = false
 
+
             val countryId = intent.getIntExtra("countryId", 0)
             Log.d("CreateActivity", "Editing country with ID: $countryId")
             val country = CountryHelper.getCountryById(countryId, ConnectionDB)
 
-            // Llenar los campos con la información del país
+
             if (country != null) {
                 Log.d("CreateActivity", "Country found: $country")
                 editTextId.setText(country.id.toString())
@@ -52,17 +63,18 @@ class CreateActivity : AppCompatActivity() {
         }
     }
 
-    @SuppressLint("DefaultLocale")
+
     private fun setupSubmitButton() {
         val editTextId: EditText = findViewById(R.id.editTextId)
         val editTextName: EditText = findViewById(R.id.editTextNombre)
         val buttonSubmit: Button = findViewById(R.id.buttonSubmit)
 
+
         buttonSubmit.setOnClickListener {
             val id = editTextId.text.toString().toIntOrNull()
             val name = editTextName.text.toString()
 
-            // Validar campos
+
             if (id == null) {
                 Toast.makeText(this, "Por favor, ingrese un ID válido.", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
@@ -72,31 +84,33 @@ class CreateActivity : AppCompatActivity() {
                 return@setOnClickListener
             }
 
-            // Crear objeto Country con los datos ingresados
+
             val country = Country(id, name)
 
-            // Insertar o actualizar el país según si ya existe o no
+
             if (CountryHelper.getCountryById(id, ConnectionDB) != null) {
                 if (CountryHelper.updateCountry(country, ConnectionDB)) {
                     Toast.makeText(this, "País actualizado correctamente", Toast.LENGTH_SHORT).show()
-                    val resultIntent = Intent()
-                    resultIntent.putExtra("actualizado", true)
-                    setResult(Activity.RESULT_OK, resultIntent)
-                    finish()
+                    setResultAndFinish(true)
                 } else {
                     Toast.makeText(this, "Error al actualizar el país", Toast.LENGTH_SHORT).show()
                 }
             } else {
                 if (CountryHelper.insertCountry(country, ConnectionDB)) {
                     Toast.makeText(this, "País insertado correctamente", Toast.LENGTH_SHORT).show()
-                    val resultIntent = Intent()
-                    resultIntent.putExtra("creado", true)
-                    setResult(Activity.RESULT_OK, resultIntent)
-                    finish()
+                    setResultAndFinish(false)
                 } else {
                     Toast.makeText(this, "Error al insertar el país", Toast.LENGTH_SHORT).show()
                 }
             }
         }
+    }
+
+
+    private fun setResultAndFinish(isUpdated: Boolean) {
+        val resultIntent = Intent()
+        resultIntent.putExtra(if (isUpdated) "actualizado" else "creado", true)
+        setResult(Activity.RESULT_OK, resultIntent)
+        finish()
     }
 }
