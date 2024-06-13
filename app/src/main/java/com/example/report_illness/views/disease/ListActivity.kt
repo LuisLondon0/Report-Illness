fb3c577f2041309f1a7a70e5c48623c3b4cf05eb
Esteban Henao <86729091+estebanhv@users.noreply.github.com>
2024-06-13 00:55:01 -0500

    Refactorizacion de metodos, evitando metodos muy largos en listar enfermedades

--- app/src/main/java/com/example/report_illness/views/disease/ListActivity.kt
+++ app/src/main/java/com/example/report_illness/views/disease/ListActivity.kt
@@ -1,10 +1,10 @@
 package com.example.report_illness.views.disease
 
-import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -12,7 +12,6 @@ import com.example.report_illness.R
 import com.example.report_illness.adapters.DiseaseAdapter
 import com.example.report_illness.database.ConnectionDB
 import com.example.report_illness.helpers.DiseaseHelper
-import androidx.activity.result.contract.ActivityResultContracts
 
 class ListActivity : AppCompatActivity() {
 
@@ -24,39 +23,25 @@ class ListActivity : AppCompatActivity() {
         const val CREATE_LIST_REQUEST_CODE = 1
     }
 
-    @SuppressLint("MissingInflatedId", "WrongViewCast")
+    private val createActivityLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            handleActivityResult(result.data)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_disease_list)
 
         ConnectionDB.dbConn()
+        initializeViews()
+        setupDiseaseList()
+        setupButtons()
+    }
 
+    private fun initializeViews() {
         recyclerViewDiseases = findViewById(R.id.recyclerViewDiseases)
         recyclerViewDiseases.layoutManager = LinearLayoutManager(this)
-
-        setupDiseaseList()
-
-        val buttonGoToMenu = findViewById<Button>(R.id.buttonGoToMenu)
-        buttonGoToMenu.setOnClickListener {
-            finish()
-        }
-
-        val buttonCreateDisease = findViewById<Button>(R.id.buttonCreateDisease)
-        buttonCreateDisease.setOnClickListener {
-            val intent = Intent(this@ListActivity, CreateActivity::class.java)
-            startActivityForResult(intent, CREATE_LIST_REQUEST_CODE)
-        }
-
-        val activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == Activity.RESULT_OK) {
-                val data = result.data
-                val actualizado = data?.getBooleanExtra("actualizado", false) ?: false
-                val creado = data?.getBooleanExtra("creado", false) ?: false
-                if (actualizado || creado) {
-                    updateDiseaseList()
-                }
-            }
-        }
     }
 
     private fun setupDiseaseList() {
@@ -65,23 +50,36 @@ class ListActivity : AppCompatActivity() {
         recyclerViewDiseases.adapter = diseaseAdapter
     }
 
+    private fun setupButtons() {
+        findViewById<Button>(R.id.buttonGoToMenu).setOnClickListener {
+            finish()
+        }
+
+        findViewById<Button>(R.id.buttonCreateDisease).setOnClickListener {
+            val intent = Intent(this@ListActivity, CreateActivity::class.java)
+            startActivityForResult(intent, CREATE_LIST_REQUEST_CODE)
+        }
+    }
+
+    private fun handleActivityResult(data: Intent?) {
+        val isUpdated = data?.getBooleanExtra("actualizado", false) ?: false
+        val isCreated = data?.getBooleanExtra("creado", false) ?: false
+        if (isUpdated || isCreated) {
+            updateDiseaseList()
+        }
+    }
+
     fun updateDiseaseList() {
         val updatedDiseases = DiseaseHelper.getAllDiseases(ConnectionDB)
         diseaseAdapter.updateList(updatedDiseases)
-        recyclerViewDiseases.adapter = diseaseAdapter // Vuelve a asignar el adaptador al RecyclerView
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == UPDATE_LIST_REQUEST_CODE || requestCode == CREATE_LIST_REQUEST_CODE) {
-            if (resultCode == Activity.RESULT_OK) {
-                val actualizado = data?.getBooleanExtra("actualizado", false) ?: false
-                val creado = data?.getBooleanExtra("creado", false) ?: false
-                if (actualizado || creado) {
-                    updateDiseaseList()
-                }
-            }
+        if ((requestCode == UPDATE_LIST_REQUEST_CODE || requestCode == CREATE_LIST_REQUEST_CODE) &&
+            resultCode == Activity.RESULT_OK) {
+            handleActivityResult(data)
         }
     }
 }
